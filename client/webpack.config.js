e8c8d66483c3586861aa97ddcbbb3d388b679077
Sinky Li <cityslicker86@gmail.com>
2023-05-03 14:30:42 -0700

    install.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
@@ -18,14 +19,33 @@ module.exports = () => {
       //generates html file
       new HtmlWebpackPlugin({
         template: './index.html',
+        title: 'Webpack Plugin'
       }),
+      new MiniCssExtractPlugin(),
       //injects service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
       //creates manifest file
-      new WebpackPwaManifest({}),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just another text editor',
+        short_name: 'JATE',
+        description: 'A simple text editor app',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
 
     ],
 
@@ -33,7 +53,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.m?js$/,
